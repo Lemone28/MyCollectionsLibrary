1852b61ed1d6fe48cdb5b7657a3da138d71cc282
Lemone28 <lemone681@gmail.com>
2023-05-25 16:04:00 +0300

    Create MyArrayList class

---
+++ src/main/java/com/myUnit/MyArrayList.java
@@ -0,0 +1,164 @@
+package com.myUnit;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class MyArrayList<T> implements MyList<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] elements;
+    private int size;
+
+    public MyArrayList() {}
+    public MyArrayList(int capacity) {}
+    public MyArrayList(Collection<? extends T> coll) {}
+
+    @Override
+    public boolean some(Predicate<? super T> predicate) {
+        return false;
+    }
+
+    @Override
+    public boolean all(Predicate<? super T> predicate) {
+        return false;
+    }
+
+    @Override
+    public <R> void map(Function<? super T, ? extends R> function) {
+
+    }
+
+    @Override
+    public <R> void map(Function<? super T, ? extends R> function, Predicate<? super T> predicate) {
+
+    }
+
+    @Override
+    public void action(Consumer<? super T> consumer) {
+
+    }
+
+    @Override
+    public void action(Consumer<? super T> consumer, Predicate<? super T> predicate) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
